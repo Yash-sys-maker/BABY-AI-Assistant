e0e5ae7b17b1b4b53c0179f9ff64afecbfce0c76
Yash BIsht <yashbisht044@gmail.com>
2026-03-13 23:10:01 +0530

    Update comments and print statement in main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import pyttsx3
 # 1. Initialize the 'Speaker' engine
 engine = pyttsx3.init()
 
-# 2. Set the speaking speed (150 is a good natural pace)
+# 2. Setting the speaking speed 
 engine.setProperty('rate', 150)
 
-# 3. Choose a voice (0 is usually a male voice, 1 is usually female)
+# 3. Choosing a voice 
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id) 
 
 def speak(text):
-    print(f"NOVA: {text}") # This lets you see what she says
+    print(f"NOVA: {text}") # This lets me see what she says
     engine.say(text)
     engine.runAndWait()
 
-# 4. Test it!
+
 speak("Hello! I am Nova, your offline assistant. My systems are starting up.")
